8f75bd82d241f6df1cbe5d483357a50b4cc366a0
Srinjoy Banerjee <srinjoybanerjee313@gmail.com>
2023-09-01 22:39:02 +0530

    Added files

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace UiPath_REFrameWork_UiDemo
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
